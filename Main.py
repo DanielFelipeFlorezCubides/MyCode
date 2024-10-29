215defda8135af934f3a602b4cb4d4aa0dc13dc6
DanielFelipeFlorezCubides <186082651+DanielFelipeFlorezCubides@users.noreply.github.com>
2024-10-29 12:01:30 -0500

    Pidiendo datos al usuario

--- Main.py
+++ Main.py
@@ -1,20 +1,10 @@
-name = input('Tell me your name: ')
-sentence = (f'hello world {name}')
-print(sentence)
+fullName = input('Put your full name: ')
+age = int(input('Put your age: ')) # Input function only save what you write as a string, if you need something else aside of a string we have to convert it.
+gender = input('Put your possible gender options, M, F, X: ')
+height = float(input('Put your height: '))
 
-name = 'Daniel Felipe Florez Cubides' # Variable string type
-age = 20 # Variable int type
-height = 1.781 # Variable double type
-trainer = False # Varible tipo boolean
-hobbies = ['Programacion', 'Deportes', 'Musica'] # Variable list or array type
-direction = (dict) ["country": "Colombia", "State": "Santander"] # Object type
-
-# The things you cannot do with variables types
-# nombre 1 = miguel//
-# try to combine different types, example: print("Pi") is still a string, not int type
-
-print(name, '\n', age) # '\n' is used for create the same function as enter in the same print line
-
-print(f"""
-    Student's name: {name}\n    Student's age: {age}
-""")                            # Correct way to concatenate and keep a clean code
+print(f""" 
+    Hello user {fullName}, how are you? Python greets you.
+    Thanks for share your age {age}, I didn't know you were a human {gender} type
+    and your height were {height}.
+""")
